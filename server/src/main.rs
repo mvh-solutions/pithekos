04de1bffbcc3a19cdba15170cc41df931823587b
Mark Howe <mvahowe@gmail.com>
2024-11-27 17:17:25 +0100

    Flat i18n endpoints

--- server/src/main.rs
+++ server/src/main.rs
@@ -376,6 +376,97 @@ async fn negotiated_i18n(state: &State<AppSettings>, filter: PathBuf) -> status:
     }
 }
 
+#[get("/flat/<filter..>")]
+async fn flat_i18n(state: &State<AppSettings>, filter: PathBuf) -> status::Custom<(ContentType, String)> {
+    let path_to_serve = state.resources_dir.clone() + os_slash_str() + "i18n.json";
+    let filter_items: Vec<String> = filter.display().to_string().split('/').map(String::from).collect();
+    if filter_items.len() > 2 {
+        return status::Custom(
+            Status::BadRequest,
+            (
+                ContentType::JSON,
+                make_bad_json_data_response(format!("expected 0 - 2 filter terms, not {}", filter_items.len()).to_string())
+            ),
+        )
+    }
+    let mut type_filter: Option<String> = None;
+    let mut subtype_filter: Option<String> = None;
+    if filter_items.len() > 0 && filter_items[0] != "" {
+        type_filter = Some(filter_items[0].clone());
+        if filter_items.len() > 1 && filter_items[1] != "" {
+            subtype_filter = Some(filter_items[1].clone());
+        }
+    }
+    match fs::read_to_string(path_to_serve) {
+        Ok(v) => {
+            match serde_json::from_str::<Value>(v.as_str()) {
+                Ok(sj) => {
+                    let languages = state.languages.clone();
+                    let mut flat = Map::new();
+                    for (i18n_type, subtypes) in sj.as_object().unwrap() {
+                        // println!("{}", i18n_type);
+                        match type_filter.clone() {
+                            Some(v) => {
+                                if v != *i18n_type {
+                                    continue;
+                                }
+                            },
+                            None => {}
+                        }
+                        for (i18n_subtype, terms) in subtypes.as_object().unwrap() {
+                            // println!("   {}", i18n_subtype);
+                            match subtype_filter.clone() {
+                                Some(v) => {
+                                    if v != *i18n_subtype {
+                                        continue;
+                                    }
+                                },
+                                None => {}
+                            }
+                            for (i18n_term, term_languages) in terms.as_object().unwrap() {
+                                // println!("      {}", i18n_term);
+                                'user_lang: for user_language in languages.clone() {
+                                    for (i18n_language, translation) in term_languages.as_object().unwrap() {
+                                        // println!("{} {}", i18n_language, languages[0]);
+                                        if *i18n_language == user_language {
+                                            let flat_key = format!("{}:{}:{}", i18n_type.clone(), i18n_subtype.clone(), i18n_term.clone());
+                                            flat.insert(flat_key, translation.clone());
+                                            break 'user_lang;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    status::Custom(
+                        Status::Ok,
+                        (
+                            ContentType::JSON,
+                            serde_json::to_string(&flat).unwrap()
+                        ),
+                    )
+
+                },
+                Err(e) => {
+                    status::Custom(
+                        Status::BadRequest,
+                        (
+                            ContentType::JSON,
+                            make_bad_json_data_response(format!("could not parse for negotiated i18n: {}", e).to_string())
+                        ),
+                    )                }
+            }
+        }
+        Err(e) => status::Custom(
+            Status::BadRequest,
+            (
+                ContentType::JSON,
+                make_bad_json_data_response(format!("could not read for negotiated i18n: {}", e).to_string())
+            ),
+        )
+    }
+}
+
 // REPO OPERATIONS
 
 #[get("/list-local-repos")]
@@ -1016,7 +1107,8 @@ fn rocket() -> _ {
         ])
         .mount("/i18n", routes![
             raw_i18n,
-            negotiated_i18n
+            negotiated_i18n,
+            flat_i18n
         ])
         .mount("/git", routes![
             fetch_repo,
