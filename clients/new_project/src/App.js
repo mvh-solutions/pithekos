4615b3b85c84cabed4048acafe8c8bcf2d84314a
Mark Howe <mvahowe@gmail.com>
2024-12-25 18:01:34 +0100

    App

--- clients/new_project/src/App.js
+++ clients/new_project/src/App.js
@@ -2,10 +2,10 @@ import {Grid2} from "@mui/material";
 
 function App() {
     return <Grid2 container spacing={2}>
-                <Grid2 size={12}>
-                    <h1>New Project (stub content)</h1>
-                </Grid2>
-            </Grid2>
+        <Grid2 size={12}>
+            <h1>New Project (stub content)</h1>
+        </Grid2>
+    </Grid2>
 }
 
 export default App;
